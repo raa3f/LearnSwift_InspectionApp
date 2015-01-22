d0f45298da5e428a08fc970cfaad879e58850d7c
raa3f <raaef.khan@me.com>
2015-01-23 01:06:22 +1100

    Initial Commit

---
+++ InspectionApp1_5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InspectionApp1_5
+//
+//  Created by Raaef Khan on 23/01/2015.
+//  Copyright (c) 2015 Raaef Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
